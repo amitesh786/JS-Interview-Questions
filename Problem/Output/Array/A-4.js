28aeff026a9ac6fb61f5899474c35db278c66b57
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-03-08 08:53:44 +0100

    added more array problems

---
+++ Problem/Output/Array/A-4.js
@@ -0,0 +1,17 @@
+// Check if an Array Contains a Duplicate
+
+function hasDuplicate(arr) {
+    return new Set(arr).size !== arr.length;
+  }
+  
+  console.log(hasDuplicate([1, 2, 3, 4])); // false
+  console.log(hasDuplicate([1, 2, 3, 3])); // true
+
+// Check if an Array Contains a no duplication
+
+function hasNoDuplication(arr) {
+  return new Set(arr).size === arr.length;
+}
+
+console.log(hasNoDuplication([1, 2, 3, 4])); // true
+console.log(hasNoDuplication([1, 2, 3, 3])); // false
---
+++ Problem/Output/Array/A-6.js
@@ -0,0 +1,8 @@
+// Flatten a Nested Array
+
+function flattenArray(arr) {
+  return arr.flat(Infinity);
+}
+  
+console.log(flattenArray([1, [2, [3, 4], 5]]));
+// [1, 2, 3, 4, 5]
