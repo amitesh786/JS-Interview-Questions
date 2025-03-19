dac836cf75fef7f324bfee4282a3b01247d94c53
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-03-19 19:41:02 +0100

    add more problem related to operator

---
+++ Problem/Output/Operator/O-8.js
@@ -0,0 +1,6 @@
+// Operator
+
+let num = 10;
+let result = num++ + ++num + num++;
+console.log(result); // 34
+console.log(num); // 13
---
+++ Problem/Output/Operator/O-9.js
@@ -0,0 +1,8 @@
+// Operator
+
+let s = "5";
+console.log(++s); // 6
+console.log(s++); // 6
+
+console.log(++s + s++);  // 16
+
