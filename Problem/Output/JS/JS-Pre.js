25cf062b21f0b9abacb19bbff0b752fe8d7fca85
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-04-02 19:50:40 +0200

    pre invoked problem

--- Problem/Output/JS/JS-Pre.js
+++ Problem/Output/JS/JS-Pre.js
@@ -1,15 +1,22 @@
 // Output
 
 let count = 1;
-console.log("Count Before:", count);
+console.log("Count Before:", count); // 1
 
 let funct = (count++, count++, count => count + 1);
 
-console.log("Count After :", count);
-console.log("Value Funct:", funct(count));
+console.log("Count After :", count); // 3
+console.log("Value Funct:", funct(count)); // 4
 
 // Final Output:
 
 // Count Before: 1
 // Count After : 3
 // Value Funct: 4
+
+// let count = 1;
+
+// let CountFn = (count++, count++, count => count + 1)(count);
+
+// console.log("Function:", CountFn); // 4
+
