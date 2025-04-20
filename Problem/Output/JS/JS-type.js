fd84bbe12e8c9096d12dc544c9fa2689dde91720
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-04-20 18:11:56 +0200

    optional problem

---
+++ Problem/Output/JS/JS-type.js
@@ -0,0 +1,22 @@
+// What will be the output ?
+
+var test = 1;
+
+if (function fn(){}) {
+	test += typeof fn;
+}
+console.log(test);
+
+// if (function fn(){})
+// is a function expression, not a declaration. So fn is not defined in the outer scope.
+
+// any function defined like this evaluates to a truthy value, because it's just an object.
+// if (function fn(){}) // → truthy → condition passes
+// So we enter the if block.
+
+// if (function fn(){})	Function is truthy → if runs
+
+// typeof fn	"undefined" (fn is not defined in this scope)
+
+// test += typeof fn	test = "1undefined"
+
