102d7493209ce8b80e425cecbd049169e3bb3c48
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-07-02 17:21:28 +0200

    fn access

--- Function/NamedFnExpression.js
+++ Function/NamedFnExpression.js
@@ -8,3 +8,13 @@
 // and immediately pass it to .map(). 
 // However, the name fn is only available inside the function itself,
 // not in the outer scope.
+
+// How to make it accessible
+function fn(num) {
+  return num * num;
+}
+
+const result = [1, 2, 3].map(fn);
+console.log("result:", result); // [1, 4, 9]
+
+console.log(fn(4)); // 16
