3ee03e6b20f053edd4ee85f5bf8dac6b825f2075
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-08-24 12:07:57 +0200

    modify

--- Function/SumStrict.js
+++ Function/SumStrict.js
@@ -8,14 +8,9 @@ console.log(sum(10, 10));
 
 // Important Notes
 // In non-strict mode ("use strict" is NOT enabled):
-
 // arguments reflects changes to function parameters.
-
 // So, updating arg1 = 20 also updates arguments[0], making the result 30.
-
 // In strict mode ("use strict"):
-
 // The arguments object is not linked to function parameters.
 // Changing arg1 = 20 won't change arguments[0].
-
 // So, the result would be 10 + 10 = 20.
