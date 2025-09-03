6589b4072fc6538a989f6710fabe0aa715aae647
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-09-03 19:27:36 +0200

    read only

--- Problem/Output/TS/TSReadOnly.ts
+++ Problem/Output/TS/TSReadOnly.ts
@@ -1,5 +1,4 @@
 // Question: Define a TypeScript object with readonly and optional properties.
-
 interface Product {
     readonly id: number;
     name: string;
@@ -10,4 +9,3 @@ interface Product {
 const product: Product = { id: 101, name: "Laptop" };
 // product.id = 102; // Error: Cannot assign to 'id' because it is a read-only property.
 console.log(product);
-
