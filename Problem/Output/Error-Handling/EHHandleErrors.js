9f205ae99e28dcf8c580d7a3ebc86156033ff49a
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-09-07 10:47:46 +0200

    handler error

--- Problem/Output/Error-Handling/EHHandleErrors.js
+++ Problem/Output/Error-Handling/EHHandleErrors.js
@@ -1,7 +1,5 @@
 // How do you handle errors in async/await?
-
 // Use try...catch to handle errors in async/await functions.
-
 async function fetchData() {
   try {
     let response = await fetch("https://invalid.url");
