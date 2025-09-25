96819b312d87acb9ae91f6a7a4057f3267065a08
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-09-25 20:15:33 +0200

    hoisting

--- Problem/Output/JS/Hoisting.js
+++ Problem/Output/JS/Hoisting.js
@@ -8,7 +8,6 @@ var a = 10;
 // Explanation:
 
 // Variable Hoisting
-
 // JavaScript hoists variable declarations (var), but not their assignments.
 
 var a; // Hoisted declaration (initially undefined)
@@ -17,7 +16,6 @@ a = 20;
 console.log(a); // Prints 20
 
 // Execution:
-
 // console.log(a); → The a inside the function is hoisted but uninitialized, so it prints undefined.
 // var a = 20; → a is assigned 20.
 // console.log(a); → Prints 20.
