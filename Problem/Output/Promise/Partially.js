3237be163fd6fddedfb4f5d93b297588f90b1470
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-11-18 06:45:31 +0100

    partially promise js

--- Problem/Output/Promise/Partially.js
+++ Problem/Output/Promise/Partially.js
@@ -7,14 +7,12 @@ const p3 = Promise.resolve("Another Success");
 Promise.allSettled([p1, p2, p3]).then(console.log);
 
 // Answer
-
 [
-    { status: "fulfilled", value: "Success" },
-    { status: "rejected", reason: "Failure" },
-    { status: "fulfilled", value: "Another Success" }
-]
-  
+  { status: "fulfilled", value: "Success" },
+  { status: "rejected", reason: "Failure" },
+  { status: "fulfilled", value: "Another Success" },
+];
+
 // Explanation:
 
 // Unlike Promise.all(), Promise.allSettled() returns results of all Promises, even if some fail.
-
