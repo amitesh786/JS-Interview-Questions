a458cad5ad2ed4749ab1d11854bd75ce0f33adb4
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-11-26 20:19:44 +0100

    anagrams array

--- Problem/Output/Array/Anagrams.js
+++ Problem/Output/Array/Anagrams.js
@@ -1,6 +1,7 @@
 // Check if Two Strings are Anagrams
 
-const isAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
-  
+const isAnagram = (str1, str2) =>
+  str1.split("").sort().join("") === str2.split("").sort().join("");
+
 console.log(isAnagram("listen", "silent")); // true
 console.log(isAnagram("hello", "world")); // false
