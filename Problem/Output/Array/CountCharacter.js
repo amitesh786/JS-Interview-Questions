a4813f97fcfaf4b91d502e59012dd4601413ba39
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-11-28 08:11:41 +0100

    count character js

--- Problem/Output/Array/CountCharacter.js
+++ Problem/Output/Array/CountCharacter.js
@@ -2,6 +2,6 @@
 
 let countChar1 = (str, char) => str.split(char).length - 1;
 
-let countChar2 = (str, char) => [...str].filter(c => c === char).length;
+let countChar2 = (str, char) => [...str].filter((c) => c === char).length;
 
 console.log(countChar1("hello world", "l")); // 3
