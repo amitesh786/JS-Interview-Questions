048b55d4ebdd8190c5362d4cd26f17d13d916174
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-12-01 10:28:30 +0100

    factorial array js

--- Problem/Output/Array/Factorial.js
+++ Problem/Output/Array/Factorial.js
@@ -1,7 +1,7 @@
 // 7. Find the Factorial of a Number (Recursion)
 
 function factorial(n) {
-    return n === 0 ? 1 : n * factorial(n - 1);
+  return n === 0 ? 1 : n * factorial(n - 1);
 }
-  
+
 console.log(factorial(5)); // 120
