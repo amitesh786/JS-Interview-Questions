7c517d49d72a67c5e5ca815c874d31a04a2370c7
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-12-13 19:03:26 +0100

    callback problem

--- Problem/Output/Callback/Callback.js
+++ Problem/Output/Callback/Callback.js
@@ -1,23 +1,23 @@
 // Callback Problem
 
-function divideByHalf(sum){
-    console.log(Math.floor(sum / 2));
+function divideByHalf(sum) {
+  console.log(Math.floor(sum / 2));
 }
 
-function multiplyBy2(sum){
-    console.log(sum * 2);
+function multiplyBy2(sum) {
+  console.log(sum * 2);
 }
 
-function operationOnSum(num1,num2,operation){
-    var sum = num1 + num2;
-    operation(sum);
+function operationOnSum(num1, num2, operation) {
+  var sum = num1 + num2;
+  operation(sum);
 }
 
 operationOnSum(3, 3, divideByHalf); // Outputs 3
 
 operationOnSum(5, 5, multiplyBy2); // Outputs 20
 
-// The operationOnSum function takes 3 arguments, the first number, the second number, 
+// The operationOnSum function takes 3 arguments, the first number, the second number,
 // and the operation that is to be performed on their sum (callback).
 
 // Both divideByHalf and multiplyBy2 functions are used as callback functions.
