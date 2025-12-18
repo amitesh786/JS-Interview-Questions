24ccdf27846acfdc01bd4da5cbd0de6620c9a6aa
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-12-18 20:48:46 +0100

    fn js destructiuring

--- Problem/Output/Destructuring/Function.js
+++ Problem/Output/Destructuring/Function.js
@@ -3,19 +3,19 @@
 // Question: What will be the output?
 
 function sum([a, b]) {
-    return a + b;
-  }
-  console.log(sum([3, 7]));
-  console.log(sum([]));
+  return a + b;
+}
+console.log(sum([3, 7]));
+console.log(sum([]));
 
 // Answer:
 
 // 10
 // Error! Undefined values cause NaN (Not a Number)
-  
+
 // Fix: Use default values:
 function sum([a = 0, b = 0] = []) {
-    return a + b;
-  }
+  return a + b;
+}
 console.log(sum([])); // 0
 console.log(sum([5])); // 5
