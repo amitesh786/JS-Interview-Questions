72a18a588db1891d37c704788e5f39988eab4558
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-12-29 17:08:09 +0100

    even filter js

--- Problem/Output/Filter/Even.js
+++ Problem/Output/Filter/Even.js
@@ -2,7 +2,7 @@
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const evenNumbers = numbers.filter(num => num % 2 === 0);
+const evenNumbers = numbers.filter((num) => num % 2 === 0);
 
 console.log(evenNumbers); // [2, 4, 6, 8, 10]
 
