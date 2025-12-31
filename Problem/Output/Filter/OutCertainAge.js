ba1ecc1fc37f8b916ae3806c64f7244ba10b227d
Amitesh Singh <amitesh.jy.786@gmail.com>
2025-12-31 10:33:29 +0100

    certain age js

--- Problem/Output/Filter/OutCertainAge.js
+++ Problem/Output/Filter/OutCertainAge.js
@@ -1,12 +1,12 @@
 // 2. Filter Out Users Above a Certain Age
 
 const users = [
-    { name: "Alice", age: 25 },
-    { name: "Bob", age: 17 },
-    { name: "Charlie", age: 30 }
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 17 },
+  { name: "Charlie", age: 30 },
 ];
 
-const adults = users.filter(user => user.age >= 18);
+const adults = users.filter((user) => user.age >= 18);
 
 console.log(adults);
 // Output: [{ name: "Alice", age: 25 }, { name: "Charlie", age: 30 }]
