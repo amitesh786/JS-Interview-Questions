d97f8e9a8b354017cf73ac118936bec277ac5deb
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-01-02 11:06:41 +0100

    unique filter js

--- Problem/Output/Filter/Unique.js
+++ Problem/Output/Filter/Unique.js
@@ -2,7 +2,9 @@
 
 const numbers = [1, 2, 2, 3, 4, 4, 5];
 
-const uniqueNumbers = numbers.filter((num, index, arr) => arr.indexOf(num) === index);
+const uniqueNumbers = numbers.filter(
+  (num, index, arr) => arr.indexOf(num) === index
+);
 
 console.log(uniqueNumbers); // [1, 2, 3, 4, 5]
 
