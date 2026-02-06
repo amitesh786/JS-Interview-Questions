6c71c7c89e09b7492e1b449bd9ac5c57d54cfaac
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-02-06 09:15:57 +0100

    find words longer filter

--- Problem/Output/Filter/F-FindWordsLonger.js
+++ Problem/Output/Filter/F-FindWordsLonger.js
@@ -2,7 +2,7 @@
 
 const words = ["apple", "banana", "kiwi", "strawberry", "grape"];
 
-const longWords = words.filter(word => word.length > 5);
+const longWords = words.filter((word) => word.length > 5);
 
 console.log(longWords); // ["banana", "strawberry"]
 
