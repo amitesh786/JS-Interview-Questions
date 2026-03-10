a73e68dd6ad962d51894548e642ff5550e87401b
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-03-10 08:46:49 +0100

    js typeof problem

--- Problem/Output/JS/JS-TypeOf2.js
+++ Problem/Output/JS/JS-TypeOf2.js
@@ -5,13 +5,13 @@ console.log(typeof null);
 // console.log(typeof undefined);
 // Output: "undefined"
 
-// Explanation: 
+// Explanation:
 // The typeof operator returns "undefined" when applied to undefined, since undefined is a primitive type in JavaScript.
 
 // console.log(typeof null);
 // Output: "object"
 
-// Explanation: 
+// Explanation:
 // The typeof operator incorrectly returns "object"
 // for null due to a legacy bug in the early versions of JavaScript.
 // null is actually a primitive value, not an object.
