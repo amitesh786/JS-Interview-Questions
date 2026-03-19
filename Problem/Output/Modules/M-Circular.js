a24ea29e554b274cc9f9b64d21701ae247d57d55
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-03-19 08:38:10 +0100

    module problem

--- Problem/Output/Modules/M-Circular.js
+++ Problem/Output/Modules/M-Circular.js
@@ -1,14 +1,14 @@
 // Question 5: Circular Dependencies
 // file: a.js
-const b = require('./b');
+const b = require("./b");
 module.exports = { name: "Module A", b };
 
 // file: b.js
-const a = require('./a');
+const a = require("./a");
 module.exports = { name: "Module B", a };
 
 // file: main.js
-const a = require('./a');
+const a = require("./a");
 console.log(a.b.name);
 
 // What will be the output? How does Node.js handle circular dependencies?
