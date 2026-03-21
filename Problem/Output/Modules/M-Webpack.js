9618af558f87e34b2cb4af774df3fceed5f518c5
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-03-21 09:05:54 +0100

    module webpack problem

--- Problem/Output/Modules/M-Webpack.js
+++ Problem/Output/Modules/M-Webpack.js
@@ -1,11 +1,11 @@
 // Webpack Entry and Output
 // webpack.config.js
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: __dirname + '/dist'
-    }
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist",
+  },
 };
 
 // What does Webpack do when bundling files with this configuration?
