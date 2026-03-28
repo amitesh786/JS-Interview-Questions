4527f383a5550c71572ad25920bb2c31c11212bd
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-03-28 07:41:40 +0100

    object numberkey issue

--- Problem/Output/Object/O-NumberKeys.js
+++ Problem/Output/Object/O-NumberKeys.js
@@ -1,8 +1,8 @@
 // To calculate the number of keys in an object (obj)
 
-let obj = { 
-    "name": "Amitesh Singh",
-    "location": "Milan, Italy"
+let obj = {
+  name: "Amitesh Singh",
+  location: "Milan, Italy",
 };
 
 // Method 1: Object.keys()
@@ -11,9 +11,9 @@ console.log("Object Key lengt (Method 1):", Object.keys(obj).length); // Output:
 // Method 2: for...in loop with counter
 let count = 0;
 for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-        count++;
-    }
+  if (obj.hasOwnProperty(key)) {
+    count++;
+  }
 }
 console.log("Object Key length (Method 2):", count); // Output: 2
 
@@ -21,4 +21,7 @@ console.log("Object Key length (Method 2):", count); // Output: 2
 console.log("Object Key length (Method 3):", Object.entries(obj).length); // Output: 2
 
 // Method 4: Object.getOwnPropertyNames()
-console.log("Object Key length (Method 4):", Object.getOwnPropertyNames(obj).length); // Output: 2
+console.log(
+  "Object Key length (Method 4):",
+  Object.getOwnPropertyNames(obj).length,
+); // Output: 2
