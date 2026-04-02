f1086d041b99e74f5defc4f25ff1bda18974529d
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-04-02 14:27:06 +0200

    object prototype of

--- Problem/Output/Object/O-IsPrototypeOf.js
+++ Problem/Output/Object/O-IsPrototypeOf.js
@@ -1,6 +1,6 @@
 // Car is a constructor function.
 function Car() {}
-// In JavaScript, functions have a special property called prototype, 
+// In JavaScript, functions have a special property called prototype,
 // which is used to set up prototype inheritance for objects created by that function.
 
 const tesla = new Car();
@@ -24,7 +24,7 @@ console.log(Car.prototype.__proto__ === Object.prototype); // true
 // Car.prototype itself is an object.
 // All objects in JavaScript (except Object.prototype itself) inherit from Object.prototype.
 // So, Car.prototype.__proto__ === Object.prototype is true.
-// That means Object.prototype is in the prototype chain of Car.prototype, 
+// That means Object.prototype is in the prototype chain of Car.prototype,
 // so Object.prototype.isPrototypeOf(Car.prototype) returns true.
 
 // isPrototypeOf() checks if an object exists in another's prototype chain.
