e5fbf82f1ed88f35c361f2769b44d42e0e92ca21
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-04-06 10:54:25 +0200

    object prototype of

--- Problem/Output/Object/O-SetPrototypeOf.js
+++ Problem/Output/Object/O-SetPrototypeOf.js
@@ -1,15 +1,19 @@
 // What is Object.setPrototypeOf() used for?
 
-// The Object.setPrototypeOf(obj, proto) method sets the prototype of obj to proto. 
+// The Object.setPrototypeOf(obj, proto) method sets the prototype of obj to proto.
 // This allows you to change an object’s prototype after it has been created.
 
 const animal = { sound: "Generic sound" };
-const dog = { bark: function() { console.log("Woof!"); } };
+const dog = {
+  bark: function () {
+    console.log("Woof!");
+  },
+};
 
 Object.setPrototypeOf(dog, animal);
 
 console.log(dog.sound); // Output: Generic sound (inherited from animal)
-dog.bark();             // Output: Woof!
+dog.bark(); // Output: Woof!
 
-// Note: Using Object.setPrototypeOf() is not recommended for performance reasons, 
+// Note: Using Object.setPrototypeOf() is not recommended for performance reasons,
 // as modifying an object's prototype dynamically can slow down JavaScript engines.
