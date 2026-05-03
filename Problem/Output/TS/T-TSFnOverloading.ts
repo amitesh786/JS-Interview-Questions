81b2bb4934f1b0d4483b2051c82973ae4faeb1f4
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-05-03 12:55:26 +0200

    ts fn overloading

--- Problem/Output/TS/T-TSFnOverloading.ts
+++ Problem/Output/TS/T-TSFnOverloading.ts
@@ -3,7 +3,7 @@
 function add(a: number, b: number): number;
 function add(a: string, b: string): string;
 function add(a: any, b: any): any {
-    return a + b;
+  return a + b;
 }
 
 // Example usage
