32a5dd67bd30670d1f84f0d283769c22dc5a2b2a
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-05-09 09:25:22 +0200

    ts promise delay

--- Problem/Output/TS/T-TSPromiseDelay.ts
+++ Problem/Output/TS/T-TSPromiseDelay.ts
@@ -1,8 +1,8 @@
 // Question: Write a function that returns a promise resolving after 2 seconds.
 function delayedMessage(message: string, delay: number): Promise<string> {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(message), delay);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(message), delay);
+  });
 }
 
 // Example usage
