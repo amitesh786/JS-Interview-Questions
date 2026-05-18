4a0d7192685cd30e66ec787cbdf24a8001d39d1a
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-05-18 09:27:16 +0200

    function expression

--- Function/F-NamedFnExpression.js
+++ Function/F-NamedFnExpression.js
@@ -1,11 +1,11 @@
 // How to access fn function, is it possible ?
 
-[1,2,3].map(function fn(num) {
-	return num * num;
+[1, 2, 3].map(function fn(num) {
+  return num * num;
 });
 
-// You define a named function expression (fn), 
-// and immediately pass it to .map(). 
+// You define a named function expression (fn),
+// and immediately pass it to .map().
 // However, the name fn is only available inside the function itself,
 // not in the outer scope.
 
