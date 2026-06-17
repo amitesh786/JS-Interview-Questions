1971d8b38e3a475add98efbf21d08b6a5ebed1f2
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-06-17 09:06:35 +0200

    array longest word

--- Problem/Output/Array/A-LongestWord.js
+++ Problem/Output/Array/A-LongestWord.js
@@ -5,7 +5,7 @@ const longestWord = (sentence) =>
     .split(" ")
     .reduce(
       (longest, word) => (word.length > longest.length ? word : longest),
-      ""
+      "",
     );
 
 console.log(longestWord("The quick brown fox jumps over the lazy dog")); // "jumps"
