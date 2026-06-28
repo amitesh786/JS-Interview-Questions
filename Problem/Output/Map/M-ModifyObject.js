f4e4adee8eeb48c55395a77c9f951247d24636c3
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-06-28 18:30:27 +0200

    map object issue

--- Problem/Output/Map/M-ModifyObject.js
+++ Problem/Output/Map/M-ModifyObject.js
@@ -1,13 +1,13 @@
 // 6. Modify an Array of Objects Conditionally
 const students = [
-    { name: "Alice", score: 85 },
-    { name: "Bob", score: 60 },
-    { name: "Charlie", score: 95 }
+  { name: "Alice", score: 85 },
+  { name: "Bob", score: 60 },
+  { name: "Charlie", score: 95 },
 ];
 
-const updatedStudents = students.map(student => ({
-    ...student,
-    passed: student.score >= 70
+const updatedStudents = students.map((student) => ({
+  ...student,
+  passed: student.score >= 70,
 }));
 
 console.log(updatedStudents);
