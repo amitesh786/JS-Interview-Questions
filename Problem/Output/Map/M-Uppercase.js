06991e460b2bd9b73588a9b7938932db0a5923a5
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-07-03 10:51:31 +0200

    map uppercase issue

--- Problem/Output/Map/M-Uppercase.js
+++ Problem/Output/Map/M-Uppercase.js
@@ -1,6 +1,6 @@
 // 3. Convert an Array of Strings to Uppercase
 const words = ["hello", "world", "javascript"];
 
-const upperCaseWords = words.map(word => word.toUpperCase());
+const upperCaseWords = words.map((word) => word.toUpperCase());
 console.log(upperCaseWords);
 // ["HELLO", "WORLD", "JAVASCRIPT"]
