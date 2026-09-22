7f23ed39dc481c2a5760247a5184dad9eedc8159
Amitesh Singh <amitesh.jy.786@gmail.com>
2026-09-22 19:56:24 +0200

    react count

--- Problem/React/Counter/React-Count-6.jsx
+++ Problem/React/Counter/React-Count-6.jsx
@@ -1,15 +1,15 @@
 const App = () => {
-    const [count, setCount] = React.useState(0);
-  
-    React.useEffect(() => {
-      setTimeout(() => {
-        console.log(count);
-      }, 3000);
-    }, []);
-  
-    return <h1>{count}</h1>;
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      console.log(count);
+    }, 3000);
+  }, []);
+
+  return <h1>{count}</h1>;
 };
-  
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 // Options:
@@ -20,12 +20,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 // Explanation
 
-// The useEffect hook has an empty dependency array ([]), 
+// The useEffect hook has an empty dependency array ([]),
 // meaning it only runs once, when the component mounts.
 
 // Inside useEffect, there is a setTimeout function that logs count after 3 seconds.
 
-// Since count is 0 at the time of the initial render, 
+// Since count is 0 at the time of the initial render,
 // the logged value will be 0—even if count updates later,
 // because the callback inside setTimeout captures the initial state value (closure behavior).
 
